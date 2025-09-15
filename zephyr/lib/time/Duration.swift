5f41ad71ac22c6fee20da66a979be2b539ec7d0f
Schubert Anselme <schubert@anselm.es>
2025-09-15 10:06:45 -0400

    refactor: Introduce Time module with Duration and Instant types for Zephyr
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ zephyr/lib/time/Duration.swift
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+// Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
+
+import ZephyrSys
+
+extension Duration: TimeoutConvertible {}
+
+public extension Duration {
+  /// Get the duration as system ticks
+  var ticks: Tick {
+    let totalNanoSeconds = self.components.seconds * 1_000_000_000 + Int64(self.components.attoseconds / 1_000_000)
+    let ticksPerNanoSecond = Double(SYS_FREQUENCY) / 1_000_000_000.0
+    return Tick(Double(totalNanoSeconds) * ticksPerNanoSecond)
+  }
+
+  /// Create a Duration from system ticks
+  init(ticks: Tick) {
+    let seconds = Double(ticks) / Double(SYS_FREQUENCY)
+    self = .seconds(seconds)
+  }
+
+  /// Convert to Timeout
+  func toTimeout() -> Timeout { Timeout(from: self) }
+}
