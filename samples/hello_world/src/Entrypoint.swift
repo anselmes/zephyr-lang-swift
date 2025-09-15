f0b507e50b443b7f95635e2f51d6511bd4ac27fc
Schubert Anselme <schubert@anselm.es>
2025-09-14 23:45:19 -0400

    fix: Correct SPDX license identifier and update sleep method in Entrypoint
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- samples/hello_world/src/Entrypoint.swift
+++ samples/hello_world/src/Entrypoint.swift
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-Licence-Identifier: GPL-3.0
 // Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
 
 import Zephyr
@@ -24,7 +24,7 @@ public struct Entrypoint {
     // should continue running until power is removed
     while true {
       // Pause execution for 1 second
-      k_msleep(1000)
+      sleep(Duration.from(milliseconds: 1000))
 
       // Print a status message to indicate the application is still alive
       print("still running...")
