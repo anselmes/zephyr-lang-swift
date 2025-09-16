d0c762b4c01c7485a4836deca0fa443e0654b7c4
Schubert Anselme <schubert@anselm.es>
2025-09-15 20:19:30 -0400

    feature: Add Swift debug info support in CMake and Swift files
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- zephyr/src/Application.swift
+++ zephyr/src/Application.swift
@@ -2,4 +2,9 @@
 // Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
 
 @_cdecl("main")
-public func main() { entrypoint() }
+public func main() {
+  #if SWIFT_DEBUG_INFO
+    print("Debug info enabled")
+  #endif
+  entrypoint()
+}
